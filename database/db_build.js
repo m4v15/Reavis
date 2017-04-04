16d11b608eaa987a7c708cfd2dbb80613e35d80a
Macintosh Helper <noreply@macintoshhelper.com>
2017-04-04 15:58:42 +0300

    setup database files for server

---
+++ database/db_build.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const dbConnection = require('./db_connection.js');
+
+fs.readFile(path.join(__dirname, './db_build.sql'), (err, file) => {
+  if (err) throw err;
+  dbConnection.query(file.toString(), (err, res) => {
+    if (err) throw err;
+    console.log('Database created with the result: ', res);
+    process.exit();
+})
---
+++ database/db_build.sql
@@ -0,0 +1,32 @@
+BEGIN;
+
+DROP TABLE IF EXISTS members, votes cascade;
+
+CREATE TABLE members (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  position	VARCHAR(100) NOT NULL,
+  location VARCHAR(100),
+  description TEXT,
+  languages	TEXT
+);
+
+CREATE TABLE votes (
+  id	PRIMARY KEY SERIAL NOT NULL,
+  member_id references members(id) INT NOT NULL,
+  num_votes	INT
+);
+
+INSERT INTO members (name, position, location, description, languages)
+VALUES ('macintoshhelper', 'student', 'Planet Earth', 'I don\'t know', 'English');
+
+INSERT INTO members (name, position, location, description, languages)
+VALUES ('Suuuuuha', 'student', 'Nazareth', 'BLIMEY!', 'Arabic, English');
+
+INSERT INTO members (name, position, location, description, languages)
+VALUES ('RQ16', 'student', 'Everywhere', 'self-obsessed', 'Arabic, English');
+
+INSERT INTO members (name, position, location, description, languages)
+VALUES ('mavis', 'student', 'Nazareth', 'no comment', 'only English');
+
+COMMIT;
