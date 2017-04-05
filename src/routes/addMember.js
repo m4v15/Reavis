51ccc24b4640f500ac9d99bc2938f6b27580c9d0
m4v15 <matthewpeterdavis@gmail.com>
2017-04-05 15:24:23 +0300

    remove comment

--- src/routes/addMember.js
+++ src/routes/addMember.js
@@ -10,16 +10,6 @@ const handler = (request, reply) => {
     console.log('Database INSERT INTO response: ', res);
   })
 
-  // query.getAll((err,res) => {
-  //   if(err) console.log(err);
-  //   let data = {
-  //       title: 'FACN Hapi Members',
-  //       description: 'An app which shows people involved in FACN1, where a user can see everyone involved, and add new people',
-  //       members: res
-  //     }
-  //   reply.view('index', data);
-  // })
-  // query.addMember()
   reply.redirect('/');
 }
 
