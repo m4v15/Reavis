32300297c6fd1415fff8215bce8cefa3f6b3871f
Macintosh Helper <noreply@macintoshhelper.com>
2017-04-05 14:34:08 +0300

    Add addMember handler/route to routes/index.js module.exports

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const homeHandler = require('./home.js');
 const staticHandler = require('./static.js');
+const addMemberHandler = require('./addMember.js');
 
 module.exports =[
-  homeHandler, staticHandler
+  homeHandler, staticHandler, addMemberHandler
 ];
