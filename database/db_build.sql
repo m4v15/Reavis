8aa8a416f4fc11a4670402278424063c115a71b9
Reem Qais <reemq86@hotmail.com>
2017-04-06 12:18:35 +0300

    Removed spacing in db_build.sql

--- database/db_build.sql
+++ database/db_build.sql
@@ -5,16 +5,16 @@ DROP TABLE IF EXISTS members, votes cascade;
 CREATE TABLE members (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  position	VARCHAR(100) NOT NULL,
+  position VARCHAR(100) NOT NULL,
   location VARCHAR(100),
   description TEXT,
-  languages	TEXT
+  languages TEXT
 );
 
 CREATE TABLE votes (
   id SERIAL PRIMARY KEY,
   member_id INTEGER references members (id) NOT NULL,
-  num_votes	INTEGER
+  num_votes INTEGER
 );
 
 INSERT INTO members (name, position, location, description, languages)
