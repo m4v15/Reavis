c2a58544074514ea50ba4fc59fa405603f2421d7
Macintosh Helper <noreply@macintoshhelper.com>
2017-04-06 11:55:11 +0300

    Fix indentation in src/routes/home for let data

--- src/routes/home.js
+++ src/routes/home.js
@@ -5,10 +5,10 @@ const handler = (request, reply) =>{
   query.getAll((err,res) => {
     if(err) console.log(err);
     let data = {
-        title: 'FACN Hapi Members',
-        description: 'An app which shows people involved in FACN1, where a user can see everyone involved, and add new people',
-        members: res.rows
-      }
+      title: 'FACN Hapi Members',
+      description: 'An app which shows people involved in FACN1, where a user can see everyone involved, and add new people',
+      members: res.rows
+    }
     reply.view('index', data);
   })
 };
