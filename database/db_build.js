42b20013be8e679d658db779032fa28b1ebc38a7
Matt Davis <matthewpeterdavis@gmail.com>
2017-05-08 22:07:16 +0300

    tidy up

--- database/db_build.js
+++ database/db_build.js
@@ -26,35 +26,35 @@ fs.readFile(path.join(__dirname, './db_build.sql'), (err, file) => {
   if (err) throw err;
   dbConnection.query(file.toString(), (error, res) => {
     if (error) throw error;
-    request(options, (Rerror, response, body) => {
-      let members = 0;
-      const params = [];
-      let memberArray = JSON.parse(body);
-      memberArray.forEach((user) => {
-        params.push(user.id);
-        params.push(user.login);
-        params.push(user.html_url);
-        params.push('Nazareth');
-        params.push(user.avatar_url);
-        params.push('English');
-        members += 1;
-      });
-      request(options2, (Rerror2, respons2, body2) => {
-        memberArray = JSON.parse(body2);
-        memberArray.forEach((user) => {
-          params.push(user.id);
-          params.push(user.login);
-          params.push(user.html_url);
-          params.push('Nazareth');
-          params.push(user.avatar_url);
-          params.push('English');
-          members += 1;
-        });
-        query.filldb(members, params, () => {
-          console.log(`Database made with res: ${res}`);
-          process.exit(0);
-      })
-      });
+  });
+})
+
+request(options, (Rerror, response, body) => {
+  let members = 0;
+  const params = [];
+  let memberArray = JSON.parse(body);
+  memberArray.forEach((user) => {
+    params.push(user.id);
+    params.push(user.login);
+    params.push(user.html_url);
+    params.push(user.organizations_url);
+    params.push(user.avatar_url);
+    params.push('English');
+    members += 1;
+  });
+  request(options2, (Rerror2, respons2, body2) => {
+    memberArray = JSON.parse(body2);
+    memberArray.forEach((user) => {
+      params.push(user.id);
+      params.push(user.login);
+      params.push(user.html_url);
+      params.push(user.organizations_url);
+      params.push(user.avatar_url);
+      params.push('English');
+      members += 1;
     });
+    query.filldb(members, params, () => {
+      process.exit(0);
+    })
   });
 });
